cce776894b750afe37f2462728eb4925125ad1d1
mark <markjava@foxmail.com>
2022-03-09 21:33:40 +0800

    完善诗词歌赋

--- pages/poetry/index.js
+++ pages/poetry/index.js
@@ -1,13 +1,32 @@
 Page({
     data:{
-        searchVal:''
+        searchVal:'',
+        active: 0,
+        categoryList:[
+          {
+            id:1,
+            title:'唐诗'
+          },
+          {
+            id:2,
+            title:'宋词'
+          },
+          {
+            id:4,
+            title:'元曲'
+          }
+        ]
     },
     actionSearch( ){
         const keyword = this.selectComponent('#searchText')
         let val = keyword.data.value;
         this.search(val);
     },
-    
+    onChange(event) {
+        let title = event.detail.title;
+        let index = event.detail.index;
+        console.log(title,index)
+      },
     onCofirmSearch(e){
         let val = e.detail;
         this.search(val);
