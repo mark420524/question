6425f7563da69f724b1bbedb22f204bb08f711cf
mark <markjava@foxmail.com>
2022-03-17 12:49:04 +0800

    增加自定义title

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -5,7 +5,18 @@ Page({
     onLoad(){
         let item = wx.getStorageSync('poetryItem' );
         console.log(item)
-        
+        wx.setNavigationBarTitle({
+          title: item.title,
+        })
         this.setData({item:item})
     },
+    onHide: function() {
+       console.log('onhide')
+      },
+    onUnload: function() {
+        this.unloadData();
+    },
+    unloadData(){
+        wx.removeStorageSync( 'poetryItem'  );
+    }
 })
