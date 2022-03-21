4c94744a3ec089bbbb785ba8bf5d966c6f49d329
mark <markjava@foxmail.com>
2022-03-21 18:02:00 +0800

    增加校验token

--- api/http.js
+++ api/http.js
@@ -9,7 +9,8 @@ const http = (params) => {
       url: apiUrl + params.url,//服务器url+参数中携带的接口具体地址
       data: params.data,//请求参数
       header: params.header || {
-        "Content-Type": "application/x-www-form-urlencoded"//设置后端需要的常用的格式就好，特殊情况调用的时候单独设置
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-token-zaowanda":""
       },
       method: params.method || 'POST',//默认为POST,可以不写，如常用请求格式为POST，可以设置POST为默认请求方式
       dataType: params.dataType,//返回的数据格式,默认为JSON，特殊格式可以在调用的时候传入参数
@@ -42,6 +43,7 @@ const http = (params) => {
 function errorToast(message){
   wx.showToast({
     title:  message ,
+    icon:'none'
   })
 }
 module.exports = {
