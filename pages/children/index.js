91262c1135b882f9caa560b972e1c9a81c53c6f4
mark <markjava@foxmail.com>
2022-03-24 22:04:37 +0800

    蒙学页面太丑了唉

--- pages/children/index.js
+++ pages/children/index.js
@@ -1,3 +1,22 @@
+const db = wx.cloud.database()
 Page({
-    data:{}
+    data:{},
+    handlerChildren(e){
+        let type = e.currentTarget.dataset.type;
+        db.collection('children').limit(1)
+        .where({
+          type:type
+        })
+        .get({
+          success: function(res) {
+            let item =  res.data[0];
+            wx.setStorageSync('poetryItem', item)
+            wx.navigateTo({
+                url: '/pages/detail/index',
+            })
+          }
+        }
+        );
+        
+    }
 })
