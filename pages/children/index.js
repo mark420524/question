1740ed635da9831af5a24156a3d9de2892189ef3
mark <markjava@foxmail.com>
2022-03-26 10:24:42 +0800

    优化页面loading

--- pages/children/index.js
+++ pages/children/index.js
@@ -1,7 +1,12 @@
 const db = wx.cloud.database()
+const app = getApp();
+const utils = app.utils;
 Page({
     data:{},
     handlerChildren(e){
+      wx.showLoading({
+        title: '查询中',
+      })
         let type = e.currentTarget.dataset.type;
         db.collection('children').limit(1)
         .where({
@@ -9,11 +14,16 @@ Page({
         })
         .get({
           success: function(res) {
+            wx.hideLoading( );
             let item =  res.data[0];
             wx.setStorageSync('poetryItem', item)
             wx.navigateTo({
                 url: '/pages/detail/index',
             })
+          },
+          fail: function(res){
+            wx.hideLoading( );
+            utils.showWxToast('查询失败');
           }
         }
         );
