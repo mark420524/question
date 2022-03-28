e12fa6b7e899f6db5f0f26c7eded0da20ec925aa
mark <markjava@foxmail.com>
2022-03-28 20:12:22 +0800

    完善赠送积分获取

--- pages/share/invite.js
+++ pages/share/invite.js
@@ -1,3 +1,39 @@
+const app = getApp();
+const utils = app.utils;
+const db = wx.cloud.database();
 Page({
-    data:{}
+    data:{
+        integral:0,
+    },
+    onLoad(){
+        let that = this;
+        db.collection('sys_dict').limit(1).where({ 
+            type: 'share_integral',
+            status:1
+          })
+          .get({
+              success:res=>{
+                  console.log(res)
+                  if (res.data && res.data.length>0){
+                    let item = res.data[0];
+                    that.setData({
+                        integral:item.value
+                    })
+                  }else{
+                    utils.showWxToast('很遗憾，赠送积分功能已下线');
+                  }
+              },
+              fail:res=>{
+                  utils.showWxToast('读取赠送积分失败');
+              }
+          });
+    },
+    onShareAppMessage(){
+        let uid = utils.getUserId();
+        return {
+            title:'亲爱的，来这里答题喽',
+            imageUrl:'', 
+            path:'/pages/index/index?uid='+uid 
+        }
+    },
 })
