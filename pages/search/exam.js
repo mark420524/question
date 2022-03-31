3bccc29c444e210826644c899793d7ad5c668951
mark <markjava@foxmail.com>
2022-03-31 20:18:00 +0800

    增加树形菜单选择

--- pages/search/exam.js
+++ pages/search/exam.js
@@ -12,6 +12,47 @@ Page({
         type:1,
         totalSize:0,
         showFilter:false,
+        mainActiveIndex: 0,
+        activeId: null,
+        items:[
+          {
+            // 导航名称
+            text: '所有城市',
+            
+            children: [
+              {
+                // 名称
+                text: '温州',
+                // id，作为匹配选中状态的标识
+                id: 1,
+                
+              },
+              {
+                text: '杭州',
+                id: 2,
+              },
+            ],
+          },
+          {
+            // 导航名称
+            text: '所有城市2',
+            
+            // 该导航下所有的可选项
+            children: [
+              {
+               
+                text: '温州1',
+               
+                id: 3,
+                
+              },
+              {
+                text: '杭州1',
+                id: 4,
+              },
+            ],
+          },
+        ]
     },
     onLoad( ){ 
       this.init( );
@@ -144,5 +185,18 @@ Page({
     },
     onClickHide() {
       this.setData({ showFilter: false });
+    },
+    onClickNav({ detail = {} }) {
+      this.setData({
+        mainActiveIndex: detail.index || 0,
+      });
+      return false;
+    },
+  
+    onClickItem({ detail = {} }) {
+      const activeId = this.data.activeId === detail.id ? null : detail.id;
+  
+      this.setData({ activeId });
+      return false;
     },
 })
