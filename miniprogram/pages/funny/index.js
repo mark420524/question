0424715555ab62a14ccda11f2452d8db2f1d01a8
mark <markjava@foxmail.com>
2022-04-16 19:06:49 +0800

    娱乐天地页面

--- miniprogram/pages/funny/index.js
+++ miniprogram/pages/funny/index.js
@@ -1,10 +1,12 @@
 Page({
     data:{},
     onLoad(){
+        /*
         wx.cloud.callFunction({
             name: "querytv"
         }).then(res=>{
             console.log(res.result)
         }).catch(console.error)
+        */
     }
 })
